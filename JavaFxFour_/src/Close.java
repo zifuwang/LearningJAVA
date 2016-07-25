a183e5404cfa339f85e83e03958b3305cb65b7df
Zifu <zifuwang@gmail.com>
2016-07-25 09:22:50 -0700

    test1

--- JavaFxFour_/src/Close.java
+++ JavaFxFour_/src/Close.java
@@ -1,59 +1,51 @@
 
-	import javafx.stage.*;
-	import javafx.scene.*;
-	import javafx.scene.layout.*;
-	import javafx.scene.control.*;
-	import javafx.geometry.*;
-	import javafx.application.Application;
-	import javafx.event.ActionEvent;
-	import javafx.event.EventHandler;
-	import javafx.scene.Scene;
-	import javafx.scene.control.Button;
-	import javafx.scene.control.Label;
-	import javafx.scene.layout.StackPane;
-	import javafx.scene.layout.VBox;
-	import javafx.stage.Stage;
-	import javafx.scene.Scene;
-	
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-	public class Close extends Application {
-		Stage window;
-		Button button1;
-		
+public class Close extends Application {
+	Stage window;
+	Button button1;
 
-	
-		@Override 
-	public void start(Stage primaryStage){
+	@Override
+	public void start(Stage primaryStage) {
 		window = primaryStage;
 		window.setTitle("Title");
 		window.setOnCloseRequest(e -> {
 			e.consume();
-			closeProgram();	
+			closeProgram();
 		});
-		
+
 		button1 = new Button("Close new window");
 		button1.setOnAction(e -> closeProgram());
-		
+
 		StackPane layout1 = new StackPane();
 		layout1.getChildren().add(button1);
 		Scene scene1 = new Scene(layout1, 300, 250);
-		
+
 		window.setScene(scene1);
 		window.show();
-		}
+	}
 
-	private void closeProgram(){
+	private void closeProgram() {
 		boolean answer = ComfirmBox.display("Alert!", "Are you sure you want to close this window?");
-		if(answer)
+		if (answer)
 			window.close();
-		
-		}
-	public static void display(String title, String message){
+
+	}
+
+	public static void display(String title, String message) {
 		Stage window = new Stage();
 		window.initModality(Modality.APPLICATION_MODAL);
 		window.setTitle(title);
 		window.setMinWidth(250);
-		
+
 		Label label1 = new Label();
 		label1.setText(message);
 		Button closeButton = new Button("Close this window");
@@ -62,11 +54,9 @@
 		layout1.getChildren().addAll(label1, closeButton);
 		layout1.setAlignment(Pos.CENTER);
 
-		//GIT comparison testing
+		// GIT comparison testing
 		Scene scene = new Scene(layout1);
 		window.setScene(scene);
 		window.showAndWait();
 	}
 }
-
-
